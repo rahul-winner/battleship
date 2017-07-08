a0c04a07109c29d0aae72f08a61efa5cb033c18f
rahul_winner <rahul.winner@gmail.com>
2017-07-08 23:33:30 +0530

    Sample ship service and its backend (#2)
    * - adding bootstrap
    - putting up sample table display
    
    * adding a sample grid
    
    * adding services
    
    * adding ships backend module

--- views/app/app.component.ts
+++ views/app/app.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { ShipService } from './ships/ships.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'Battleship';
+  private subscriptionsList: Array<Subscription> = [];
+  constructor(private shipService: ShipService) {
+
+  }
 
   selectCell(row, col) {
+    const coordinate: string = row + col;
     console.log('cell = ' + row + col);
+    this.subscriptionsList.push(this.shipService.hitLocation(coordinate).subscribe((res) => {
+      console.log(res);
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptionsList.forEach(subscription => {
+      subscription.unsubscribe();
+    });
   }
 }
--- views/app/app.module.ts
+++ views/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ShipService } from './ships/ships.service';
+import { MarkDirective } from './core/mark.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarkDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [ShipService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
